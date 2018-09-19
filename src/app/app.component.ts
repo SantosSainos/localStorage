184374920c56fddb6c57387e8f0cc4641dbf6045
SantosSainos <sainosher@hotmail.com>
2018-09-18 21:20:08 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'localStorage';
+
+  constructor() {
+    // this.grabar_localstorage();
+    this.obtener_localstorage();
+  }
+
+
+  obtener_localstorage() {
+    const nombre = localStorage.getItem('nombre');
+    const persona = JSON.parse(localStorage.getItem('persona'));
+
+    console.log(nombre);
+    console.log(persona);
+  }
+
+  grabar_localstorage() {
+    const nombre = 'sainos';
+
+    const persona = {
+      nombre: 'juan',
+      edad: 28,
+      coords: {
+        lat: 10,
+        alt: -10
+      }
+    };
+
+    localStorage.setItem('nombre', nombre);
+    localStorage.setItem('persona', JSON.stringify( persona ));
+  }
 }
